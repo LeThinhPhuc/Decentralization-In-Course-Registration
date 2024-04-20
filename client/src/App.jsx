07875b61cf97786382ee8954f9ee7f9758ad5dcd
LeThinhPhuc <phuclethinh70@gmail.com>
2024-04-20 22:39:11 +0700

    first

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import ListAccount from './components/ListAccount/ListAccount'
+
+function App() {
+
+  return (
+    <>
+      <ListAccount/>
+    </>
+  )
+}
+
+export default App
---
+++ client/src/components/ListAccount/ListAccount.jsx
@@ -0,0 +1,6 @@
+const ListAccount = () =>{
+    return(
+        <div className="text-red-500 text-center">hello</div>
+    )
+}
+export default ListAccount;
