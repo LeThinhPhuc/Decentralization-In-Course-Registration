b6ba90ec43bce76aeb1482cd2274c39581d4d071
KallaneKoe <92024724+KallaneKoe@users.noreply.github.com>
2024-04-23 17:36:51 +0700

    first

--- client/src/components/ListAccount/ListAccount.jsx
+++ client/src/components/ListAccount/ListAccount.jsx
@@ -1,6 +1,299 @@
-const ListAccount = () =>{
-    return(
-        <div className="text-red-500 text-center">hello</div>
-    )
-}
+import React from "react";
+
+const ListAccount = () => {
+  return (
+    <div className=" text-center ">
+      <div
+        id="static-modal"
+        data-modal-backdrop="static"
+        tabindex="-1"
+        aria-hidden="true"
+        class=" overflow-y-auto overflow-x-auto fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
+      >
+        <div class="relative p-4 w-full max-w-[70vw] ">
+          <div class="relative bg-gray-100 rounded-lg shadow-xl ">
+            <div class="flex items-center justify-between p-4 md:p-5 border-b border-gray-300 rounded-t ">
+              <h3 class="text-xl font-semibold text-gray-900 ">Static modal</h3>
+              <button
+                type="button"
+                class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center "
+                data-modal-hide="static-modal"
+              >
+                <svg
+                  class="w-3 h-3"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 14 14"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                  />
+                </svg>
+                <span class="sr-only">Close modal</span>
+              </button>
+            </div>
+
+            <div class="relative overflow-x-auto">
+              <table class="w-full text-sm text-left rtl:text-right text-gray-500 ">
+                <thead class="text-xs text-gray-700 uppercase bg-gray-50 ">
+                  <tr>
+                    <th scope="col" class="px-6 py-3">
+                      MSSV
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                      Họ và tên
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                      Số điện thoại
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                      Điểm hệ số 1
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                      Diểm hệ số 2
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                      Diểm hệ số 3
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                      Diểm trung bình
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr class="bg-white border-b ">
+                    <th
+                      scope="row"
+                      class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
+                    >
+                      47.01.104.100
+                    </th>
+                    <td class="px-6 py-4">Nguyễn Văn A</td>
+                    <td class="px-6 py-4">12345678</td>
+                    <td>
+                      <input
+                        type="grade"
+                        class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                        placeholder=" "
+                        required
+                      />
+                    </td>
+                    <td>
+                      <input
+                        type="grade"
+                        class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                        placeholder=" "
+                        required
+                      />
+                    </td>
+                    <td>
+                      <input
+                        type="grade"
+                        class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                        placeholder=" "
+                        required
+                      />
+                    </td>
+                  </tr>
+                  <tr class="bg-white border-b ">
+                    <th
+                      scope="row"
+                      class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
+                    >
+                      47.01.104.101
+                    </th>
+                    <td class="px-6 py-4">Nguyễn Văn B</td>
+                    <td class="px-6 py-4">12345678</td>
+                    <td>
+                      <input
+                        type="grade"
+                        class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                        placeholder=" "
+                        required
+                      />
+                    </td>
+                    <td>
+                      <input
+                        type="grade"
+                        class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                        placeholder=" "
+                        required
+                      />
+                    </td>
+                    <td>
+                      <input
+                        type="grade"
+                        class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                        placeholder=" "
+                        required
+                      />
+                    </td>
+                  </tr>
+                  <tr class="bg-white border-b">
+                    <th
+                      scope="row"
+                      class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                    >
+                      47.01.104.102
+                    </th>
+                    <td class="px-6 py-4">Nguyễn Văn C</td>
+                    <td class="px-6 py-4">12345678</td>
+                    <td>
+                      <input
+                        type="grade"
+                        class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                        placeholder=" "
+                        required
+                      />
+                    </td>
+                    <td>
+                      <input
+                        type="grade"
+                        class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                        placeholder=" "
+                        required
+                      />
+                    </td>
+                    <td>
+                      <input
+                        type="grade"
+                        class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                        placeholder=" "
+                        required
+                      />
+                    </td>
+                  </tr>
+                  <tr class="bg-white border-b">
+                    <th
+                      scope="row"
+                      class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                    >
+                      47.01.104.103
+                    </th>
+                    <td class="px-6 py-4">Nguyễn Văn D</td>
+                    <td class="px-6 py-4">12345678</td>
+                    <td>
+                      <input
+                        type="grade"
+                        class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                        placeholder=" "
+                        required
+                      />
+                    </td>
+                    <td>
+                      <input
+                        type="grade"
+                        class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                        placeholder=" "
+                        required
+                      />
+                    </td>
+                    <td>
+                      <input
+                        type="grade"
+                        class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                        placeholder=" "
+                        required
+                      />
+                    </td>
+                  </tr>
+                  <tr class="bg-white border-b">
+                    <th
+                      scope="row"
+                      class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                    >
+                      47.01.104.104
+                    </th>
+                    <td class="px-6 py-4">Nguyễn Văn E</td>
+                    <td class="px-6 py-4">12345678</td>
+                    <td>
+                      <input
+                        type="grade"
+                        class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                        placeholder=" "
+                        required
+                      />
+                    </td>
+                    <td>
+                      <input
+                        type="grade"
+                        class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                        placeholder=" "
+                        required
+                      />
+                    </td>
+                    <td>
+                      <input
+                        type="grade"
+                        class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                        placeholder=" "
+                        required
+                      />
+                    </td>
+                  </tr>
+                  <tr class="bg-white border-b">
+                    <th
+                      scope="row"
+                      class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                    >
+                      47.01.104.105
+                    </th>
+                    <td class="px-6 py-4">Nguyễn Văn F</td>
+                    <td class="px-6 py-4">12345678</td>
+                    <td>
+                      <input
+                        type="grade"
+                        class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                        placeholder=" "
+                        required
+                      />
+                    </td>
+                    <td>
+                      <input
+                        type="grade"
+                        class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                        placeholder=" "
+                        required
+                      />
+                    </td>
+                    <td>
+                      <input
+                        type="grade"
+                        class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                        placeholder=" "
+                        required
+                      />
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+
+            <div class="flex items-center p-4 md:p-5 border-t border-gray-300 rounded-b ">
+              <button
+                data-modal-hide="static-modal"
+                type="button"
+                class="text-white bg-green-300 hover:bg-green-400 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center "
+              >
+                Save
+              </button>
+              <button
+                data-modal-hide="static-modal"
+                type="button"
+                class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200  hover:bg-red-500 hover:text-white focus:z-10 focus:ring-4 focus:ring-gray-100 "
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default ListAccount;
