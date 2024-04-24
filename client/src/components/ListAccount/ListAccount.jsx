1793b8cfcf31ed3021cfb1d667369351247afddc
KallaneKoe <92024724+KallaneKoe@users.noreply.github.com>
2024-04-24 19:50:10 +0700

    commit

--- client/src/components/ListAccount/ListAccount.jsx
+++ client/src/components/ListAccount/ListAccount.jsx
@@ -128,25 +128,28 @@ const ListAccount = () => {
                       <td class="px-6 py-4">{item.SDT}</td>
                       <td>
                         <input
-                          type="grade"
-                          class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                          placeholder=" "
+                          type="text"
+                          id="company"
+                          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  "
+                          placeholder="Flowbite"
                           required
                         />
                       </td>
                       <td>
                         <input
-                          type="grade"
-                          class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                          placeholder=" "
+                          type="text"
+                          id="company"
+                          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5   "
+                          placeholder="Flowbite"
                           required
                         />
                       </td>
                       <td>
                         <input
-                          type="grade"
-                          class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                          placeholder=" "
+                          type="text"
+                          id="company"
+                          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  "
+                          placeholder="Flowbite"
                           required
                         />
                       </td>
