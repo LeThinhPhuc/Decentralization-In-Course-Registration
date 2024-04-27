d77102138ce5a8aa9c314d1208b4c9345b4a0862
PhamTrieu0703 <thanhtrieupham10@gmail.com>
2024-04-27 21:56:36 +0700

    add componets ChangInfo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
 import ListAccount from './components/ListAccount/ListAccount'
-
+import ChangeInfo from './components/ChangeInfo/ChangeInfo'
 function App() {
 
   return (
     <>
-      <ListAccount/>
+      <ChangeInfo/>
     </>
   )
 }
